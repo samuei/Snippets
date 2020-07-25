2a4b1e7d7338a454f218280afba9ce58e22f9309
Samuel A. Lamb <samuei@users.noreply.github.com>
2020-07-25 18:09:37 -0500

    Update read_feed
    I decided that the `fortune` output was annoying rather than amusing. So, I got rid of it and replaced it with a minute-long podcast. Of course, that wasn't complicated enough, so I looked up more and figured out how to randomize what I get each morning.
    
    Variety is the spice, etc etc.

--- crons/read_feed.sh
+++ crons/read_feed.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
-# Takes NPR news hour feed, isolates the mp3, and plays it
+# Takes in one long news feed and one short feed, isolates the mp3s, and plays them. Also gives me the weather.
 
-rssaddy=$(wget -qO- "https://www.npr.org/rss/podcast.php?id=500005" |\
-grep -o '<enclosure url="[^?]*' |\
-grep -o '[^"]*$')
+export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games
 
-mplayer $rssaddy
+# Get long podcast
+longcast=$(shuf -n 1 ./longcastlist.txt)
+
+longrssaddy=$(wget -qO- $longcast |\
+grep -o -m 1 -P '(?<=<enclosure url=")[^"?]*')
+
+# Get short podcast
+shortcast=$(shuf -n 1 ./shortcastlist.txt)
+
+shortrssaddy=$(wget -qO- $shortcast |\
+grep -o -m 1 -P '(?<=<enclosure url=")[^"?]*')
+
+# Play one, read the weather, then play the other
+mplayer $longrssaddy
+
+./scripts/temperature.sh
+
+espeak "And now for something completely different. ." 2>/dev/null
+
+mplayer $shortrssaddy
