3e8d7668206c281f231b7f99ead4e78a40d96fd2
Samuel A. Lamb <samuei@users.noreply.github.com>
2020-08-16 22:13:10 -0500

    Force HTTPS
    One of the podcasts isn't using it, which caused it to always fail to load. Now it has no choice!

--- crons/read_feed.sh
+++ crons/read_feed.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-# Takes in one long news feed and one short feed, isolates the mp3s, and plays them. Also gives me the weather.
+# Takes in one long news feed and one short feed, isolates the mp3s, and plays them
 
 export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games
+
+insecurehttp="http:"
+securehttp="https:"
 
 # Get long podcast
 longcast=$(shuf -n 1 ./longcastlist.txt)
@@ -9,11 +12,15 @@ longcast=$(shuf -n 1 ./longcastlist.txt)
 longrssaddy=$(wget -qO- $longcast |\
 grep -o -m 1 -P '(?<=<enclosure url=")[^"?]*')
 
+longrssaddy=$(echo $longrssaddy | sed "s/$insecurehttp/$securehttp/g")
+
 # Get short podcast
 shortcast=$(shuf -n 1 ./shortcastlist.txt)
 
 shortrssaddy=$(wget -qO- $shortcast |\
 grep -o -m 1 -P '(?<=<enclosure url=")[^"?]*')
+
+shortrssaddy=$(echo $shortrssaddy | sed "s/$insecurehttp/$securehttp/g")
 
 # Play one, read the weather, then play the other
 mplayer $longrssaddy
