ecaa3e8f254b6298ce24cc74c33aed822a722dab
Samuel A. Lamb <samuei@users.noreply.github.com>
2024-05-11 17:27:47 -0500

    Update media_rename
    Try the album artist if the track's is empty and sanitize more

--- media_rename.py
+++ media_rename.py
@@ -1,6 +1,19 @@
 from tinytag import TinyTag # https://github.com/devsnd/tinytag
 import os
 
+def get_sanitized_string(original_string):
+    # Please note: This is not comprehensive
+    return original_string \
+           .replace('/', '∕') \
+           .replace('\\', '\\\\') \
+           .replace('?', '？') \
+           .replace('*', '⁎') \
+           .replace(':', '꞉') \
+           .replace('<', '＜') \
+           .replace('>', '＞') \
+           .replace('"', '″') \
+           .strip()
+
 def rename_files(path_to_media):
     # Replace arbitrary filenames with Artist - Song Title in specified folder
     # Example input: rename_files('C:\\Users\\doug\\Music')
@@ -23,17 +36,23 @@ def rename_files(path_to_media):
         # Ignore non-mp3 files:
         if extension == '.mp3' or extension == '.m4a':
             tag = TinyTag.get(file_name)
-            if tag.artist is None or tag.title is None:
             # Ignore files without the necessary metadata:
+            if (tag.artist is None and tag.albumartist is None) or tag.title is None:
                 files_skipped += 1
                 continue
             
-            # Kludgey? Yes. Deals with Simon & Garfunkel without crashing? Yes.
-            # Windows also forbids some other characters. Beware.
+            if tag.artist is None:
+                artist_name = tag.albumartist
+            else:
+                artist_name = tag.artist
+
+            artist_name = get_sanitized_string(artist_name)
+            new_title = get_sanitized_string(tag.title)
+            
             os.rename(file_name,
-                      tag.artist.replace('/','∕').replace('\\','\\\\')
+                      artist_name
                       + ' - '
-                      + tag.title.replace('/','∕').replace('\\','\\\\')
+                      + new_title
                       + extension)
             files_renamed += 1
     
